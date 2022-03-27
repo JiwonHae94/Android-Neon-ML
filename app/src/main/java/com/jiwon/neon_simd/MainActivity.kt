155b61cc6272ca5d1bdca1820a4258ca9ff5fa44
JiwonHae <hjiwon1994@gmail.com>
2022-03-28 03:35:37 +0900

    FEAT : declare cpp link files FEAT : create jni class and methods for comparison

--- app/src/main/java/com/jiwon/neon_simd/MainActivity.kt
+++ app/src/main/java/com/jiwon/neon_simd/MainActivity.kt
@@ -2,10 +2,19 @@ package com.jiwon.neon_simd
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<TextView>(R.id.)
+    }
+
+    external fun stringFromJNI() : String
+
+    init{
+        System.loadLibrary("native-lib")
     }
 }
